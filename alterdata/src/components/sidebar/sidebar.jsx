bf42582048432be026a387df30891ddb723e0750
MathesPCardoso <matheuspcardoso@gmail.com>
2022-07-02 09:59:47 -0300

    0.3

--- alterdata/src/components/sidebar/sidebar.jsx
+++ alterdata/src/components/sidebar/sidebar.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react'
 
 import { ProSidebar, SidebarHeader, SidebarFooter, Menu, MenuItem, SubMenu, SidebarContent } from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
-
 import { BiUser, BiCheckCircle } from "react-icons/bi";
 import { GoMarkGithub } from "react-icons/go";
 import { Button } from 'reactstrap';
@@ -50,8 +49,10 @@ export default function Sidebar() {
         </SidebarContent>
         <SidebarFooter style={{ textAlign: 'center' }}>
           <div>
+            <a href="https://github.com/MatheusPCardoso/Alterdata">
             <Button
               style={{ margin: "15px 0", borderRadius: "20px", backgroundColor: "rgba(255,255,255,0.15)" }}
+              onClick={() => {}}
             >
               <div style={{ display: "flex", justifyContent: 'center', alignItems: 'center' }}>
                 <GoMarkGithub />
@@ -60,6 +61,8 @@ export default function Sidebar() {
                 </div>
               </div>
             </Button>
+            </a>
+            
           </div>
         </SidebarFooter>
       </ProSidebar>
